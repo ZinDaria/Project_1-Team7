81fbb6893fd59165e111ec68c006ed504abca2a6
Valentyna K <uzarevich@ukr.net>
2023-10-04 13:52:29 -0400

    code cleaning

---
+++ api_keys.py
@@ -0,0 +1,7 @@
+# OpenWeatherMap API Key
+weather_api_key = "18c9591568d28dda8ee91552ff919ba3"
+
+weather_api_key_res = "8da3117b1a0f67387e9e280409e2a6c9"
+
+# Geoapify API Key
+geoapify_key = "8c3df386ff96481f82ad858f391d87d2"
